95c0906b6bb1928b40ea75f07ae1e6d974d9e118
TheConfusedGuy <89684850+TheConfusedGuy@users.noreply.github.com>
2025-11-10 22:59:00 +0700

    Restructure

--- source/server/configs/database.py
+++ source/server/configs/database.py
@@ -2,10 +2,7 @@
 import certifi
 from pymongo.mongo_client import MongoClient
 
-client = MongoClient(
-    'mongodb://localhost:27017/',
-    tlsCAFile=certifi.where()
-)
+client = MongoClient('mongodb://localhost:27017/')
 
 try:
     client.admin.command('ping')
